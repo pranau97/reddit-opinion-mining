d9cc8cc14c93fe21ac4cf8c30e3006e484db740a
pranau97 <pranau97@gmail.com>
2017-11-06 01:12:23 +0530

    Add frequency distriutions

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import sys
 import csv
 import math
 import numpy as np
+import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
@@ -25,17 +26,17 @@ def run_sentiment_analysis():
             total_count = 0
 
             for row in post_reader:
-                result = sia.polarity_scores(row[0])
+                resultult = sia.polarity_scoresult(row[0])
                 print("Analysing post rows: " + str(row_count + 1), end="\r")
                 row_count += 1
                 total_count += 1
 
-                if result['compound'] > 0.2:
+                if resultult['compound'] > 0.2:
                     with open(r"positive_list.txt", "a", encoding="utf-8") as outfile_posts_0:
                         outfile_posts_0.write(row[0] + "\n")
                         positive_count += 1
 
-                elif result['compound'] < -0.2:
+                elif resultult['compound'] < -0.2:
                     with open(r"negative_list.txt", "a", encoding="utf-8") as outfile_posts_1:
                         outfile_posts_1.write(row[0] + "\n")
                         negative_count += 1
@@ -43,18 +44,18 @@ def run_sentiment_analysis():
 
             row_count = 0
             for row in comment_reader:
-                result = sia.polarity_scores(row[0])
+                resultult = sia.polarity_scoresult(row[0])
                 print("Analysing comment rows: " +
                       str(row_count + 1), end="\r")
                 row_count += 1
                 total_count += 1
 
-                if result['compound'] > 0.2:
+                if resultult['compound'] > 0.2:
                     with open(r"positive_list.txt", "a", encoding="utf-8") as outfile_comments_0:
                         outfile_comments_0.write(row[0] + "\n")
                         positive_count += 1
 
-                elif result['compound'] < -0.2:
+                elif resultult['compound'] < -0.2:
                     with open(r"negative_list.txt", "a", encoding="utf-8") as outfile_comments_1:
                         outfile_comments_1.write(row[0] + "\n")
                         negative_count += 1
@@ -81,7 +82,35 @@ def plot_word_types(total_count, negative_count, positive_count, string=""):
     plt.ylabel("Percentage")
     plt.show()
 
-# def word_distribution(filename):
+
+def word_distribution(filename):
+    '''Plots words distributions for a given file.'''
+    tokenizer = RegexpTokenizer(r'\w+')
+    stop_words = set(stopwords.words('english'))
+    all_words = []
+
+    with open(filename, "r", encoding='utf-8') as f_pos:
+        for line in f_pos.readlines():
+            words = tokenizer.tokenize(line)
+            for w in words:
+                if w.lower() not in stop_words and w.lower() != "n":
+                    all_words.append(w.lower())
+    result = nltk.FreqDist(all_words)
+    print(result.most_common(8))
+
+    plt.style.use('ggplot')
+
+    y_val = [x[1] for x in result.most_common(len(all_words))]
+    y_final = []
+    for i, k, z, t in zip(y_val[0::4], y_val[1::4], y_val[2::4], y_val[3::4]):
+        y_final.append(math.log(i + k + z + t))
+    x_val = [math.log(i + 1) for i in range(len(y_final))]
+
+    plt.xlabel("Words (Log)")
+    plt.ylabel("Frequency (Log)")
+    plt.title("Word Frequency Distribution")
+    plt.plot(x_val, y_final)
+    plt.show()
 
 
 def run_topical_analysis(string):
@@ -108,39 +137,39 @@ def run_topical_analysis(string):
                 if row[6] == "''":
                     match_post_selftext = 0
                     if match_post_flair >= 85 or match_post_title >= 85:
-                        result_0 = sia.polarity_scores(row[0])
-                        result_1 = None
+                        resultult_0 = sia.polarity_scoresult(row[0])
+                        resultult_1 = None
                         include_list.append(row[7])
                     else:
                         continue
                 else:
                     match_post_selftext = fuzz.partial_ratio(string, row[6])
                     if match_post_flair >= 85 or match_post_title >= 85 or match_post_selftext >= 85:
-                        result_0 = sia.polarity_scores(row[0])
-                        result_1 = sia.polarity_scores(row[4])
+                        resultult_0 = sia.polarity_scoresult(row[0])
+                        resultult_1 = sia.polarity_scoresult(row[4])
                         include_list.append(row[7])
                     else:
                         continue
 
-                if result_0['compound'] > 0.2:
+                if resultult_0['compound'] > 0.2:
                     with open(r"positive_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_posts:
                         outfile_posts.write(row[0] + "\n")
                         positive_count += 1
                         total_count += 1
 
-                elif result_0['compound'] < -0.2:
+                elif resultult_0['compound'] < -0.2:
                     with open(r"negative_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_posts:
                         outfile_posts.write(row[0] + "\n")
                         negative_count += 1
                         total_count += 1
 
-                if result_1 is not None:
-                    if result_1['compound'] > 0.2:
+                if resultult_1 is not None:
+                    if resultult_1['compound'] > 0.2:
                         with open(r"positive_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_posts:
                             outfile_posts.write(row[0] + "\n")
                             positive_count += 1
 
-                    elif result_1['compound'] < -0.2:
+                    elif resultult_1['compound'] < -0.2:
                         with open(r"negative_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_posts:
                             outfile_posts.write(row[0] + "\n")
                             negative_count += 1
@@ -152,14 +181,14 @@ def run_topical_analysis(string):
                 if row[1] in include_list:
                     total_count += 1
 
-                    result = sia.polarity_scores(row[0])
+                    resultult = sia.polarity_scoresult(row[0])
 
-                    if result['compound'] > 0.2:
+                    if resultult['compound'] > 0.2:
                         with open(r"positive_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_comments:
                             outfile_comments.write(row[0] + "\n")
                             positive_count += 1
 
-                    elif result['compound'] < -0.2:
+                    elif resultult['compound'] < -0.2:
                         with open(r"negative_list_" + "%r" % string + r".txt", "a", encoding="utf-8") as outfile_comments:
                             outfile_comments.write(row[0] + "\n")
                             negative_count += 1
@@ -176,10 +205,16 @@ def main():
         run_sentiment_analysis()
     elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print(
-            "Usage: \n\tworkon reddit\n\tpython analysis.py [search term]|[options]")
+            "Usage: \n\tworkon reddit\n\tpython analysis.py [search term]|[option] [search term]")
+    elif sys.argv[1] == "-w" or sys.argv[1] == "--words":
+        search_string = str(sys.argv[2])
+        search_type = str(sys.argv[3])
+        filename = search_type.lower() + r"_list_" + \
+            "%r" % search_string + r".txt"
+        word_distribution(filename)
     else:
-        arg_str = str(sys.argv[1])
-        run_topical_analysis(arg_str)
+        search_string = str(sys.argv[1])
+        run_topical_analysis(search_string)
 
 
 if __name__ == '__main__':
